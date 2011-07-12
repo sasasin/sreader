e7763b2eceb59833c2ae148a1baf85cd30e6c948
Shinnosuke Suzuki <sasasin@sasasin.net>
2011-07-12 23:33:14 +0900

    up.

--- sreader/src/java/net/sasasin/sreader/FeedReader.java
+++ sreader/src/java/net/sasasin/sreader/FeedReader.java
@@ -3,9 +3,20 @@
  */
 package net.sasasin.sreader;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sasasin.sreader.ormap.FeedUrl;
+
 /**
  * @author sasasin
- *
+ * 
  */
 public class FeedReader {
 
@@ -14,12 +25,38 @@ public class FeedReader {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if (args.length < 1) {
+			return;
+		}
+
+		File path = new File(args[0]);
+		if (!path.exists()) {
+			return;
+		}
+		FeedReader fr = new FeedReader();
+		fr.importFeedList(path);
+	}
+
+	public void importFeedList(File path) {
+		try {
+			List<FeedUrl> fu = new ArrayList<FeedUrl>();
+			BufferedReader r = new BufferedReader(new FileReader(path));
+			while (r.ready()) {
+				String[] s = r.readLine().split("\t");
+				fu.add(new FeedUrl(s[0]));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	public void publishFeed() {
 
 	}
 
-	
-	public void publishFeed(){
-		
-	}
-	
 }
--- sreader/src/java/net/sasasin/sreader/ormap/FeedUrl.java
+++ sreader/src/java/net/sasasin/sreader/ormap/FeedUrl.java
@@ -5,10 +5,58 @@ package net.sasasin.sreader.ormap;
 
 /**
  * @author sasasin
- *
+ * 
  */
 public class FeedUrl {
-	public FeedUrl(){
+	private String id;
+	private String url;
+	private String authId;
+	private String authPassword;
+	private String accountId;
 
+	private FeedUrl(){
+		
+	}
+	
+	public FeedUrl(String url, String auth_id, String auth_password,
+			String accont_id) {
+		this.setUrl(url);
+		this.setAuthId(auth_id);
+		this.setAuthPassword(auth_password);
+	}
+	public FeedUrl(String url){
+		this.setUrl(url);
+	}
+
+	public String getAuthId() {
+		return authId;
+	}
+
+	public void setAuthId(String authId) {
+		this.authId = authId;
+	}
+
+	public String getAuthPassword() {
+		return authPassword;
+	}
+
+	public void setAuthPassword(String authPassword) {
+		this.authPassword = authPassword;
+	}
+
+	private String getUrl() {
+		return url;
+	}
+
+	private void setUrl(String url) {
+		this.url = url;
+	}
+
+	private String getId() {
+		return id;
+	}
+
+	private void setId(String id) {
+		this.id = id;
 	}
 }
