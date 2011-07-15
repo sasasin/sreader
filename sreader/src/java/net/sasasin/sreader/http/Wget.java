bed191d6aeaacfec4318a8c599d0f3f98cd9128e
Shinnosuke Suzuki <sasasin@sasasin.net>
2011-07-15 23:33:38 +0900

    add wget.

--- sreader/src/java/net/sasasin/sreader/http/Wget.java
+++ sreader/src/java/net/sasasin/sreader/http/Wget.java
@@ -1,5 +1,8 @@
 package net.sasasin.sreader.http;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -11,10 +14,20 @@ public class Wget {
 		this.url = url;
 	}
 
-	public String get() {
-		//TODO get url content
-		return "";
+	public String read() {
+		try {
+			BufferedReader r = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			StringBuffer s = new StringBuffer();
 
+			while (r.ready()) {
+				s.append(r.readLine());
+			}
+			return s.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 }
