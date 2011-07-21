96699f59b027d7a64d0118ac11dd9f7af6c4d75d
Shinnosuke Suzuki <sasasin@sasasin.net>
2011-07-22 02:29:33 +0900

    HTMLで配信するクラスの予定地。

---
+++ sreader/src/java/net/sasasin/sreader/publish/HtmlPublisher.java
@@ -0,0 +1,13 @@
+package net.sasasin.sreader.publish;
+
+public class HtmlPublisher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
