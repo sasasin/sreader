b2263dd1e0360e476e14343ec1ff8f483c2ef41f
Shinnosuke Suzuki <sasasin@sasasin.net>
2011-12-18 17:53:38 +0900

    findAllの見つからない場合のケースを追加。

--- sreader/src/test/java/net/sasasin/sreader/dao/impl/TestEftRulesDaoHibernateImpl.java
+++ sreader/src/test/java/net/sasasin/sreader/dao/impl/TestEftRulesDaoHibernateImpl.java
@@ -27,23 +27,24 @@ import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
+import net.sasasin.sreader.dao.EftRulesDao;
 import net.sasasin.sreader.orm.EftRules;
 import net.sasasin.sreader.testcasesupport.AbstractHibernateTestCase;
 import net.sasasin.sreader.util.Md5Util;
 
 import org.dbunit.dataset.DefaultDataSet;
+import org.dbunit.dataset.DefaultTable;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.excel.XlsDataSet;
 import org.dbunit.operation.DatabaseOperation;
 import org.hibernate.HibernateException;
 import org.hibernate.exception.ConstraintViolationException;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestEftRulesDaoHibernateImpl extends AbstractHibernateTestCase {
 
-	private EftRulesDaoHibernateImpl testee = null;
+	private EftRulesDao testee = null;
 
 	@Before
 	@Override
@@ -58,17 +59,6 @@ public class TestEftRulesDaoHibernateImpl extends AbstractHibernateTestCase {
 		DatabaseOperation.CLEAN_INSERT.execute(getConnection(), dataSet);
 	}
 
-	@After
-	@Override
-	public void tearDown() throws Exception {
-		super.tearDown();
-	}
-
-	@Override
-	protected IDataSet getDataSet() throws Exception {
-		return new DefaultDataSet();
-	}
-
 	/**
 	 * データが存在するケース。
 	 */
@@ -80,31 +70,22 @@ public class TestEftRulesDaoHibernateImpl extends AbstractHibernateTestCase {
 		assertThat(actual.size(), not(0));
 	}
 
-	//TODO 事前にデータを消す方法を考える
-	// /**
-	// * データが存在しないケース。
-	// *
-	// * @throws Exception
-	// */
-	// @Test
-	// @Ignore
-	// public void testFindAllNotFound() {
-	//
-	// // データを消す
-	// try {
-	// IDataSet dataSet = new XlsDataSet(this.getClass()
-	// .getResourceAsStream(
-	// this.getClass().getSimpleName() + ".xls"));
-	// DatabaseOperation.DELETE_ALL.execute(getConnection(), dataSet);
-	// } catch (Exception e) {
-	// // データを消すのに失敗？
-	// e.printStackTrace();
-	// }
-	//
-	// List<EftRules> actual = testee.findAll();
-	//
-	// assertThat(actual.size(), is(0));
-	// }
+	/**
+	 * データが存在しないケース。
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testFindAllNotFound() throws Exception {
+
+		// テーブルを空にする
+		DatabaseOperation.DELETE_ALL.execute(getConnection(),
+				new DefaultDataSet(new DefaultTable("eft_rules")));
+
+		List<EftRules> actual = testee.findAll();
+
+		assertThat(actual.size(), is(0));
+	}
 
 	/**
 	 * 検索対象のデータが存在するケース。
@@ -170,7 +151,7 @@ public class TestEftRulesDaoHibernateImpl extends AbstractHibernateTestCase {
 		expected.setExtractRule("");
 
 		try {
-			String actual = testee.save(expected);
+			testee.save(expected);
 			fail("Insert成功はテスト失敗");
 		} catch (ConstraintViolationException e) {
 			// 本当に入っていないか、取得してみる
@@ -210,7 +191,7 @@ public class TestEftRulesDaoHibernateImpl extends AbstractHibernateTestCase {
 		try {
 			testee.delete(expected);
 		} catch (HibernateException e) {
-			fail("Delete失敗しても例外は出ないはず");
+			fail("Delete失敗しても例外は出ない");
 		}
 	}
 
