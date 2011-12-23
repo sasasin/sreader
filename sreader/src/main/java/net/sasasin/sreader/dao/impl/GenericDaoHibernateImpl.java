145d7d6c54bfc2dbf123148e3c72459ca72d248c
Shinnosuke Suzuki <sasasin@sasasin.net>
2011-12-23 20:14:13 +0900

    不要なimportを削除。

--- sreader/src/main/java/net/sasasin/sreader/dao/impl/GenericDaoHibernateImpl.java
+++ sreader/src/main/java/net/sasasin/sreader/dao/impl/GenericDaoHibernateImpl.java
@@ -31,7 +31,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.exception.ConstraintViolationException;
 
 /**
  * {@link GenericDao}のHibernateによる実装。
