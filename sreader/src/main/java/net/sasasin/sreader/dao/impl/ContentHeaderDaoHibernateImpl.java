80d8d09ec70dfc7f7691e4acc2a4261aa2be0ffa
Shinnosuke Suzuki <sasasin@sasasin.net>
2013-01-27 20:50:54 +0900

    著作権表記を追加。

--- sreader/src/main/java/net/sasasin/sreader/dao/impl/ContentHeaderDaoHibernateImpl.java
+++ sreader/src/main/java/net/sasasin/sreader/dao/impl/ContentHeaderDaoHibernateImpl.java
@@ -1,3 +1,22 @@
+/*
+ * SReader is RSS/Atom feed reader with full text.
+ *
+ * Copyright (C) 2011, Shinnosuke Suzuki <sasasin@sasasin.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *	
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sasasin.sreader.dao.impl;
 
 import java.util.List;
@@ -32,7 +51,7 @@ public class ContentHeaderDaoHibernateImpl extends
 				.createSQLQuery(queryString).addEntity(ContentHeader.class)
 				.list();
 		// LAZYなので
-		for (ContentHeader h : listOfContentHeader){
+		for (ContentHeader h : listOfContentHeader) {
 			h.getFeedUrl();
 		}
 		return listOfContentHeader;
