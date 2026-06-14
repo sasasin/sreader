73d8976febd0b2bb278be547088fb8665e85c7d0
sasasin <sasasin@sasasin.net>
2026-06-14 19:38:25 +0900

    chore: remove legacy batch and commons modules

--- commons/src/main/java/net/sasasin/sreader/commons/dao/ContentFullTextDao.java
+++
@@ -1,7 +0,0 @@
-package net.sasasin.sreader.commons.dao;
-
-import net.sasasin.sreader.commons.entity.ContentFullText;
-
-public interface ContentFullTextDao extends GenericDao<ContentFullText, String> {
-
-}
